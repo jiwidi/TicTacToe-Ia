e4436fe355cec171d8683b72061e85729c5c815d
jiwidi <fhjaime96@gmail.com>
2016-10-21 12:41:36 +0200

    stuff

--- controlador.py
+++ controlador.py
@@ -1,19 +1,30 @@
-
+from functions import movement
+from functions import printMatrix
 w=3
 h=3 
-Matrix = [[0 for x in range(w)] for y in range(h)] 
-
+Matrix = [['-' for x in range(w)] for y in range(h)] 
+game=1
+aux=""
 print("Welcome to Tic Tac Toe Ia \n")
-
-o=1
-resul=["user","computer"]
-while(o==1):
-	print("Who start's? the computer or the user?")
-	aux=raw_input()
-	print(aux)
-	if(not (aux == "user") and not (aux == "computer")):
-		print('Wrong input')
+while(game==1):
+    o=1
+    resul=["user","computer"]
+    while(o==1):
+	    print("Who start's? the computer or the user?")
+	    aux=raw_input()
+	    if(not (aux == "user") and not (aux == "computer")):
+		    print('Wrong input')
 	
-	else:
-		o=2
-print(Matrix)
+	    else:
+		    o=2
+    if(aux=='user'):
+        print('Choose your first movement, first the X coordinate then the Y coordinate')
+        x=input()
+        y=input()
+        Matrix=movement(x,y,"o",Matrix)
+        print(Matrix)
+    else:
+    	print('The computer does his movement')
+    	Matrix=movement(1,1,"x",Matrix)
+    	print(Matrix)
+
---
+++ functions.py
@@ -0,0 +1,14 @@
+def movement( x,y,player,matrix ):
+    if(isinstance( x, int ) and isinstance( y, int )and x<3 and x>=0 and y<3 and y>=0):
+        if(matrix[x][y]=='-'):
+            matrix[x][y]=player 
+        else:
+            print("You can't place your token there, there is already one ")   
+
+    
+
+    return matrix
+def printMatrix(matrix):
+    print(matrix[0]+"\n")
+    print(matrix[1]+"\n")
+    print(matrix[2]+"\n")
