210b6e0b0d752329526edab7004ddfdaff0299e7
jiwidi <fhjaime96@gmail.com>
2016-10-22 00:46:22 +0200

    first real implementation, you can play vs ia now but there are a few bugs to fix in the interface controller yet

--- controlador.py
+++ controlador.py
@@ -1,31 +1,63 @@
 from functions import movement
 from functions import printMatrix
+from functions import getValue
+from functions import analyzeMatrix
+from functions import isComplete
 w=3
 h=3 
 Matrix = [['-' for x in range(w)] for y in range(h)] 
 game=1
 aux=""
+turn=""
 print("Welcome to Tic Tac Toe Ia \n")
 while(game==1):
     o=1
     resul=["user","computer"]
     while(o==1):
-	    print("Who start's? the computer or the user?")
-	    aux=raw_input()
-	    if(not (aux == "user") and not (aux == "computer")):
-		    print('Wrong input')
-	
-	    else:
-		    o=2
+        print("Who start's? the computer or the user?")
+        aux=raw_input()
+        if(not (aux == "user") and not (aux == "computer")):
+            print('Wrong input')
+    
+        else:
+            o=2
     if(aux=='user'):
-        print('Choose your first movement, first the X coordinate then the Y coordinate')
+        print('Choose your first movement, first the Y coordinate then the X coordinate')
         x=input()
         y=input()
         Matrix=movement(x,y,"o",Matrix)
-        print(Matrix)
+        printMatrix(Matrix)
+        turn="x"
     else:
-    	print('The computer does his movement')
-    	Matrix=movement(1,1,"x",Matrix)
-    	print(Matrix)
+        print('The computer does his movement')
+        Matrix=movement(1,1,"x",Matrix)
+        printMatrix(Matrix)
+        turn="o"
+    gam=1
+    while(gam==1):
+        if(isComplete(Matrix)):
+            print("Game end, want to restart? tipe yes or no")
+            u=input()
+            if(u=="no"):
+                game=0
+            gam=0
+
+        elif(turn=="x"):
+            print("The computer moves")
+            aux=analyzeMatrix(Matrix)
+            
+            Matrix=movement(aux[0],aux[1],"x",Matrix)
+            printMatrix(Matrix)
+            turn="o"
+        else:
+            print("Choose your next movement, first the y coordinate then the x coordinate")
+            x=input()
+            y=input()
+            Matrix=movement(x,y,"o",Matrix)
+            printMatrix(Matrix)
+            turn="x"
+
     
+    
+
 
--- functions.py
+++ functions.py
@@ -9,7 +9,55 @@ def movement( x,y,player,matrix ):
 
     return matrix
 def printMatrix(matrix):
-    print(matrix[0]+"\n")
-    print(matrix[1]+"\n")
-    print(matrix[2]+"\n")
+    print(matrix[0])
+    print(matrix[1])
+    print(matrix[2])
+
+
+def getValue(x,y,matrix):
+	result=0;
+	matrix[x][y]="x"
+	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[0][1] =="-" or matrix[0][1] =="x") and (matrix[0][2] =="-" or matrix[0][2] =="x")):
+		result+=1
+	if((matrix[1][0] =="-" or matrix[1][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[1][2] =="-" or matrix[1][2] =="x")):
+		result+=1
+	if((matrix[2][0] =="-" or matrix[2][0] =="x") and (matrix[2][1] =="-" or matrix[2][1] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result+=1
+	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[1][0] =="-" or matrix[1][0] =="x") and (matrix[2][0] =="-" or matrix[2][0] =="x")):
+		result+=1
+	if((matrix[0][1] =="-" or matrix[0][1] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[2][1] =="-" or matrix[2][1] =="x")):
+		result+=1
+	if((matrix[0][2] =="-" or matrix[0][2] =="x") and (matrix[1][2] =="-" or matrix[1][2] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result+=1
+	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result+=1
+	if((matrix[2][0] =="-" or matrix[2][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[0][2] =="-" or matrix[0][2] =="x")):
+		result+=1
+	matrix[x][y]="-"	
+	return result
+def analyzeMatrix(matrix):
+	max=0
+	dx=-1
+	dy=-1
+	aux1=-1
+	aux2=-1
+	for val in matrix:
+		aux1+=1
+		for va in val:
+			aux2+=1
+			if(va=="-"):
+				if(getValue(matrix.index(val),val.index(va),matrix)>max):
+					dx=aux1
+					dy=aux2
+		aux2=-1
+	return [dx,dy]
+
+def isComplete(matrix):
+	for val in matrix:
+		for va in val:
+			if(va=="-"):
+			    return False
+
+	return True
+
 
