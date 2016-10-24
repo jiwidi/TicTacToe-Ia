0307f7464268d8183f4b758044de0ed596cd90c1
jiwidi <fhjaime96@gmail.com>
2016-10-24 16:29:31 +0200

    lul

--- functions.py
+++ functions.py
@@ -13,27 +13,127 @@ def printMatrix(matrix):
     print(matrix[1])
     print(matrix[2])
 
-
+def duplicates(lst, item):
+  	return [i for i, x in enumerate(lst) if x == item]
+def getEmptyPos(lst):
+	if(0 in lst and 1 in lst and 2 in lst):
+		return -1
+	elif(0 in lst and 1 in lst):
+		return 2
+	elif(0 in lst and 2 in lst):
+		return 1
+	elif(1 in lst and 2 in lst):
+		return 0	
 def getValue(x,y,matrix):
 	result=0;
+	resul=[]
 	matrix[x][y]="x"
 	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[0][1] =="-" or matrix[0][1] =="x") and (matrix[0][2] =="-" or matrix[0][2] =="x")):
+		result[0]=matrix[0][0]
+		result[1]=matrix[0][1]
+		result[2]=matrix[0][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [0,0]
+			if(au==1):
+				return [0,1]
+			if(au==2):
+				return [0,2]
 		result+=1
 	if((matrix[1][0] =="-" or matrix[1][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[1][2] =="-" or matrix[1][2] =="x")):
+		result[0]=matrix[1][0]
+		result[1]=matrix[1][1]
+		result[2]=matrix[1][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [1,0]
+			if(au==1):
+				return [1,1]
+			if(au==2):
+				return [1,2]
 		result+=1
 	if((matrix[2][0] =="-" or matrix[2][0] =="x") and (matrix[2][1] =="-" or matrix[2][1] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result[0]=matrix[2][0]
+		result[1]=matrix[2][1]
+		result[2]=matrix[2][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [2,0]
+			if(au==1):
+				return [2,1]
+			if(au==2):
+				return [2,2]
 		result+=1
 	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[1][0] =="-" or matrix[1][0] =="x") and (matrix[2][0] =="-" or matrix[2][0] =="x")):
+		result[0]=matrix[0][0]
+		result[1]=matrix[1][0]
+		result[2]=matrix[2][0]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [0,0]
+			if(au==1):
+				return [1,0]
+			if(au==2):
+				return [2,0]
 		result+=1
 	if((matrix[0][1] =="-" or matrix[0][1] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[2][1] =="-" or matrix[2][1] =="x")):
+		result[0]=matrix[0][1]
+		result[1]=matrix[1][1]
+		result[2]=matrix[2][1]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [0,1]
+			if(au==1):
+				return [1,1]
+			if(au==2):
+				return [2,1]
 		result+=1
 	if((matrix[0][2] =="-" or matrix[0][2] =="x") and (matrix[1][2] =="-" or matrix[1][2] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result[0]=matrix[0][2]
+		result[1]=matrix[1][2]
+		result[2]=matrix[2][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [0,2]
+			if(au==1):
+				return [1,2]
+			if(au==2):
+				return [2,2]
 		result+=1
 	if((matrix[0][0] =="-" or matrix[0][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[2][2] =="-" or matrix[2][2] =="x")):
+		result[0]=matrix[0][0]
+		result[1]=matrix[1][1]
+		result[2]=matrix[2][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [0,0]
+			if(au==1):
+				return [1,1]
+			if(au==2):
+				return [2,2]
 		result+=1
 	if((matrix[2][0] =="-" or matrix[2][0] =="x") and (matrix[1][1] =="-" or matrix[1][1] =="x") and (matrix[0][2] =="-" or matrix[0][2] =="x")):
+		result[0]=matrix[2][0]
+		result[1]=matrix[1][1]
+		result[2]=matrix[0][2]
+		if(len(duplicates(result,"x"))==2):
+			au=getEmptyPos(len(duplicates(result,"x")))
+			if(au==0):
+				return [2,0]
+			if(au==1):
+				return [1,1]
+			if(au==2):
+				return [0,2]
 		result+=1
 	matrix[x][y]="-"	
+
 	return result
 def analyzeMatrix(matrix):
 	max=0
@@ -46,7 +146,13 @@ def analyzeMatrix(matrix):
 		for va in val:
 			aux2+=1
 			if(va=="-"):
-				if(getValue(matrix.index(val),val.index(va),matrix)>max):
+				print(aux1)
+				print(aux2)
+				if(type(getValue(aux1,aux2,matrix)) is list):
+					k=getValue(aux1,aux2,matrix)
+					return [k[0],k[1]]
+				elif(getValue(aux1,aux2,matrix)>max):
+					max=getValue(aux1,aux2,matrix)
 					dx=aux1
 					dy=aux2
 		aux2=-1
