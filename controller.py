ffaa5e6de1439a1e890bd7853a2e2aaed33fd56c
jiwidi <fhjaime96@gmail.com>
2016-10-28 12:54:24 +0200

    updated for python 3.5

--- controller.py
+++ controller.py
@@ -16,7 +16,7 @@ while(game==1):
     resul=["user","computer"]
     while(o==1):
         print("Who start's? the computer or the user?")
-        aux=raw_input()
+        aux=input()
         if(not (aux == "user") and not (aux == "computer")):
             print('Wrong input \n')
     
@@ -26,6 +26,8 @@ while(game==1):
         print('Choose your first movement, first the X coordinate then the Y coordinate \n')
         x=input()
         y=input()
+        x=int(x);
+        y=int(y);
         Matrix=movement(y-1,x-1,"o",Matrix)
         printMatrix(Matrix)
         print('The computer does his movement \n')
@@ -41,12 +43,13 @@ while(game==1):
     while(gam==1):
         if(isComplete(Matrix)):
             print("Game end, want to restart? tipe yes or no \n")
-            u=raw_input()
+            u=input()
             if(u=="no"):
                 game=0
             gam=0
 
         elif(turn=="x"):
+            print("")
             print("The computer moves \n")
             aux=analyzeMatrix(Matrix)
             Matrix=movement(aux[0],aux[1],"x",Matrix)
@@ -56,6 +59,8 @@ while(game==1):
             print("Choose your next movement, first the X coordinate then the Y coordinate \n")
             x=input()
             y=input()
+            x=int(x)
+            y=int(y)
             Matrix=movement(y-1,x-1,"o",Matrix)
             printMatrix(Matrix)
             turn="x"
